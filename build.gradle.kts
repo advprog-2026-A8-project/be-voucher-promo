3edb383fe8992a6ade2c67c5867a2a7426092544
Andi Aqsa Mappatunru Marzuki <andiaqsa365@gmail.com>
2026-02-25 16:02:37 +0700

    feat: setup initial CI/CD pipeline with SonarCloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,30 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
+    jacoco
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "advprog-2026-A8-project_be-voucher-promo")
+        property("sonar.organization", "advprog-2026-a8-project")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.jacoco.xmlReportPaths", "${project.layout.buildDirectory.get()}/reports/jacoco/test/jacocoTestReport.xml")
+    }
 }
 
 group = "id.ac.ui.cs.advprog"
