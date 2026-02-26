c85819c0907a2d67552c237d5dac4653de3e5928
Andi Aqsa Mappatunru Marzuki <andi.aqsa@ui.ac.id>
2026-02-26 22:03:53 +0700

    update file

--- src/main/java/id/ac/ui/cs/advprog/bepromovoucher/repository/VoucherRepository.java
+++
@@ -1,9 +0,0 @@
-package id.ac.ui.cs.advprog.bepromovoucher.repository;
-
-import id.ac.ui.cs.advprog.bepromovoucher.model.Voucher;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VoucherRepository extends JpaRepository<Voucher, Long> {
-}
