af0de6a5bc58d18321d2cb94c5396e9586b35982
Andi Aqsa <andiaqsa365@gmail.com>
2026-04-17 05:30:42 +0700

    add unit test for InvalidVoucherException and VoucherNotFoundException

--- src/test/java/id/ac/ui/cs/advprog/bepromovoucher/exception/InvalidVoucherExceptionTest.java
+++ src/test/java/id/ac/ui/cs/advprog/bepromovoucher/exception/InvalidVoucherExceptionTest.java
@@ -1,7 +1,22 @@
 package id.ac.ui.cs.advprog.bepromovoucher.exception;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InvalidVoucherExceptionTest {
 
+    @Test
+    void testExceptionMessage() {
+        String message = "Voucher sudah tidak berlaku";
+        InvalidVoucherException exception = new InvalidVoucherException(message);
+
+        assertEquals(message, exception.getMessage());
+    }
+
+    @Test
+    void testExceptionIsRuntimeException() {
+        InvalidVoucherException exception = new InvalidVoucherException("Test");
+
+        assertTrue(exception instanceof RuntimeException);
+    }
 }
