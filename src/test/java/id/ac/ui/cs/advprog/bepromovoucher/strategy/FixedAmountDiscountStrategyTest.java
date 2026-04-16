ff8a76923ca2c0902e2a13cb7d3e2ddecb12941f
Andi Aqsa <andiaqsa365@gmail.com>
2026-04-17 06:33:55 +0700

    add unit test for FixedAmount and Percentage Discount

--- src/test/java/id/ac/ui/cs/advprog/bepromovoucher/strategy/FixedAmountDiscountStrategyTest.java
+++ src/test/java/id/ac/ui/cs/advprog/bepromovoucher/strategy/FixedAmountDiscountStrategyTest.java
@@ -1,7 +1,35 @@
 package id.ac.ui.cs.advprog.bepromovoucher.strategy;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FixedAmountDiscountStrategyTest {
 
+    private DiscountStrategy strategy;
+
+    @BeforeEach
+    void setUp() {
+        strategy = new FixedAmountDiscountStrategy();
+    }
+
+    @Test
+    void testCalculateFixedAmountDiscount() {
+        Double originalPrice = 100000.0;
+        Double discountValue = 15000.0;
+
+        Double result = strategy.calculate(originalPrice, discountValue);
+
+        assertEquals(15000.0, result);
+    }
+
+    @Test
+    void testCalculateFixedAmountHigherThanPrice() {
+        Double originalPrice = 10000.0;
+        Double discountValue = 20000.0;
+
+        Double result = strategy.calculate(originalPrice, discountValue);
+
+        assertTrue(result <= originalPrice);
+    }
 }
