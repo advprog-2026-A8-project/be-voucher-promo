58f336b1b7bd7f1b12a0439986422a476266840d
Andi Aqsa Mappatunru Marzuki <andiaqsa365@gmail.com>
2026-05-18 03:32:21 +0700

    Add @EnableMethodSecurity to SecurityConfig to enable method-level

--- src/main/java/id/ac/ui/cs/advprog/bepromovoucher/controller/VoucherController.java
+++ src/main/java/id/ac/ui/cs/advprog/bepromovoucher/controller/VoucherController.java
@@ -7,6 +7,7 @@ import id.ac.ui.cs.advprog.bepromovoucher.service.VoucherService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
